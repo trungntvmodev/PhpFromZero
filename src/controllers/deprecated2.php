ea0ae30a4fb420d75a7dfc8106f643646be597b1
trungntvmodev <99572271+trungntvmodev@users.noreply.github.com>
2022-05-18 18:09:36 +0700

    Update deprecated2.php

--- src/controllers/deprecated2.php
+++ src/controllers/deprecated2.php
@@ -1,47 +1,5 @@
-<?php 
-  
-// Original input string 
-$original_string = "Write any topic .";
-  
-// Pattern to be searched
-$string_pattern = "(.*)any(.*)"; 
-  
-// Replace string
-$replace_string = " own yours own \\1biography\\2"; 
-  
-echo ereg_replace($patternstrVal, $replacesstrVal, $stringVal); 
-
-function __autoload($className) {
-     include $className . '.php';
-}
-
-$customClass = new CustomClass;
-
-/**
- * @deprecated  As of release 1.3, replaced by {@link #Fee}
- */
-@Deprecated
-public class Fum { ... }
-
-public class Foo {
-  /**
-   * @deprecated  As of release 1.7, replaced by {@link #doTheThingBetter()}
-   */
-  @Deprecated
-  public void doTheThing() { ... }
-
-  public void doTheThingBetter() { ... }
-}
-
-public class Bar extends Foo {
-  public void doTheThing() { ... } // Noncompliant; don't override a deprecated method or explicitly mark it as @Deprecated
-}
-
-public class Bar extends Fum {  // Noncompliant; Fum is deprecated
-
-  public void myMethod() {
-    Foo foo = new Foo();  // okay; the class isn't deprecated
-    foo.doTheThing();  // Noncompliant; doTheThing method is deprecated
-  }
-}
+<?php
+   $version = "php 4.0";
+   
+   print sql_regcase($version);
 ?>
