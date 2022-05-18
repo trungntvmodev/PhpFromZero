45394ed2699ba7f560c0874e613ea5ba3c367879
trungntvmodev <99572271+trungntvmodev@users.noreply.github.com>
2022-05-18 14:02:56 +0700

    Update inject.php

--- src/controllers/inject.php
+++ src/controllers/inject.php
@@ -1,11 +1,7 @@
 <?php
-$query  = "SELECT id, name, inserted, size FROM products
-           WHERE size = '$size'";
-$result = odbc_exec($conn, $query);
+
+$offset = $argv[0]; // beware, no input validation!
+$query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
+$result = pg_query($conn, $query);
+
 ?>
-
-The attacker can combine the static part of the query with another SELECT statement that will reveal all passwords:
-
-'
-union select '1', concat(uname||'-'||passwd) as name, '1971-01-01', '0' from usertable;
---
