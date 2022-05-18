c07e3e4d25071da27e823d64494d1992f4f22999
trungntvmodev <99572271+trungntvmodev@users.noreply.github.com>
2022-05-18 17:38:21 +0700

    Create deprecated2.php

---
+++ src/controllers/deprecated2.php
@@ -0,0 +1,14 @@
+<?php 
+  
+// Original input string 
+$original_string = "Write any topic .";
+  
+// Pattern to be searched
+$string_pattern = "(.*)any(.*)"; 
+  
+// Replace string
+$replace_string = " own yours own \\1biography\\2"; 
+  
+echo ereg_replace($patternstrVal, $replacesstrVal, $stringVal); 
+  
+?>
